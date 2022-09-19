cd38c28e81c6813a9f2522ff22abd020270446dd
milypol98 <milypol98@gmail.com>
2022-09-19 12:38:25 +0200

    First Commit

---
+++ src/main/java/pl/milypol/magazinestatus/MagazineStatusApplication.java
@@ -0,0 +1,13 @@
+package pl.milypol.magazinestatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazineStatusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagazineStatusApplication.class, args);
+    }
+
+}
