8d24d4eaf0e7de35041188d92595cc45c229d39b
milypol98 <milypol98@gmail.com>
2022-09-25 12:02:06 +0200

    user profil and privet order

--- src/main/java/pl/milypol/magazinestatus/controller/IncomingOrderController.java
+++ src/main/java/pl/milypol/magazinestatus/controller/IncomingOrderController.java
@@ -1,39 +1,45 @@
 package pl.milypol.magazinestatus.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import pl.milypol.magazinestatus.entity.IncomingOrder;
 import pl.milypol.magazinestatus.repository.IncomingOrderRepository;
+import pl.milypol.magazinestatus.repository.UserRepository;
+import pl.milypol.magazinestatus.seciurity.CurrentUser;
 
 @Controller
 @RequestMapping("/incomings")
 public class IncomingOrderController {
     private final IncomingOrderRepository incomingOrderRepository;
+    private final UserRepository userRepository;
 
-    public IncomingOrderController(IncomingOrderRepository incomingOrderRepository) {
+    public IncomingOrderController(IncomingOrderRepository incomingOrderRepository, UserRepository userRepository) {
         this.incomingOrderRepository = incomingOrderRepository;
+        this.userRepository = userRepository;
     }
 
 
     @GetMapping
-    public String IncomingOrder(Model model) {
-        model.addAttribute("orders",incomingOrderRepository.findAll());
+    public String IncomingOrder(@AuthenticationPrincipal CurrentUser customUser, Model model) {
+        model.addAttribute("orders",incomingOrderRepository.findAllByUserId(customUser.getUser().getId()));
         model.addAttribute("newOrder", new IncomingOrder());
         return "incomingOrder";
     }
     @PostMapping
-    public String addOrder(IncomingOrder order) {
+    public String addOrder(@AuthenticationPrincipal CurrentUser customUser,IncomingOrder order) {
+        order.setUser(customUser.getUser());
         incomingOrderRepository.save(order);
         return "redirect:/incomings";
     }
-    @DeleteMapping(value = "{id}")
-    public String deleteIncomingOrder(@PathVariable Long id) {
+    @DeleteMapping
+    public String deleteIncomingOrder(@RequestParam Long id) {
         incomingOrderRepository.deleteById(id);
         return "redirect:/incomings";
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/edit")
     public String getIncomingOrder(@RequestParam Long id,Model model){
         model.addAttribute("order", incomingOrderRepository.findById(id).get());
         return "editIncomingOrder";
--- src/main/java/pl/milypol/magazinestatus/repository/IncomingOrderRepository.java
+++ src/main/java/pl/milypol/magazinestatus/repository/IncomingOrderRepository.java
@@ -3,5 +3,8 @@ package pl.milypol.magazinestatus.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.milypol.magazinestatus.entity.IncomingOrder;
 
+import java.util.List;
+
 public interface IncomingOrderRepository extends JpaRepository<IncomingOrder,Long> {
+    List <IncomingOrder> findAllByUserId(Long user_id);
 }
